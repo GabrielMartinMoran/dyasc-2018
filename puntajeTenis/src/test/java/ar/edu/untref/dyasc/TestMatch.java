747641e924d2c08c426b6177e85a06cd75ab7627
Catero <cybercat_666_78@hotmail.com>
2018-10-19 05:36:11 -0300

    jugador1GanaElMatch

--- puntajeTenis/src/test/java/ar/edu/untref/dyasc/TestMatch.java
+++ puntajeTenis/src/test/java/ar/edu/untref/dyasc/TestMatch.java
@@ -41,6 +41,32 @@ public class TestMatch {
         Assert.assertEquals(0, puntajeJ2);
     }
     
+    @Test
+    public void jugador1Gana2Sets() {
+        Match match = new Match();
+
+        this.agregarPuntos(match, Jugadores.JUGADOR_1, 2*6*4);
+        int puntajeJ1 = match.getPuntaje(Jugadores.JUGADOR_1);
+        int puntajeJ2 = match.getPuntaje(Jugadores.JUGADOR_2);
+
+        Assert.assertEquals(2, puntajeJ1);
+        Assert.assertEquals(0, puntajeJ2);
+    }
+    
+    @Test
+    public void jugador1GanaElMatch() {
+        Match match = new Match();
+
+        this.agregarPuntos(match, Jugadores.JUGADOR_1, 3*6*4);
+        int puntajeJ1 = match.getPuntaje(Jugadores.JUGADOR_1);
+        int puntajeJ2 = match.getPuntaje(Jugadores.JUGADOR_2);
+        Jugadores ganador = match.getGanador();
+
+        Assert.assertEquals(3, puntajeJ1);
+        Assert.assertEquals(0, puntajeJ2);
+        Assert.assertEquals(Jugadores.JUGADOR_1, ganador);
+    }
+    
     private void agregarPuntos(Match match, Jugadores jugador, int puntos) {
         for (int i = 0; i < puntos; i++) {
             try {
